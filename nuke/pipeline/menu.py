8c08aa463e257bf01ecf2505609019972d65183f
Ned Wilson <nedwilson@gmail.com>
2018-07-03 20:09:08 -0700

    Added new functionality.

--- nuke/pipeline/menu.py
+++ nuke/pipeline/menu.py
@@ -16,6 +16,9 @@ from utilities import *
 from vclf_multi_autotrack import *
 import scaleDagNodes
 
+import delivery
+delivery.globals_from_config()
+
 import trackLinker
 
 import RotoShapes_to_trackers
@@ -35,43 +38,48 @@ import version_control
 menubar = nuke.menu("Nuke")
 m = menubar.addMenu("In-House")
 
+n = m.addMenu("&Color")
+n.addCommand("Lin2Log Wrapper", "nuke.createNode(\"LinLogWrapper.nk\")")
+
+n = m.addMenu("Delivery")
+n.addCommand("Send for Review", "send_for_review()")
+n.addCommand("Publish Delivery", "delivery.display_window(m_2k=False)")
+n.addCommand("Publish Hi-Res Delivery", "delivery.display_window(m_2k=True)")
+
 n = m.addMenu("&File")
-n.addCommand("Read from Write", "read_from_write()", "#r")
-n.addCommand("Reveal in Finder", "reveal_in_finder()", "^f")
 n.addCommand("Copy Read To Shot", "copyReadToShot()")
 n.addCommand("Copy Render To Shot","copyRenderToShot()")
+n.addCommand("Read from Write", "read_from_write()", "#r")
+n.addCommand("Reveal in Finder", "reveal_in_finder()", "^f")
+
+n = m.addMenu("&Filter")
+n.addCommand("Arri Alexa Grain", "nuke.createNode(\'ScannedGrainIH\')")
+n.addCommand("Edge Scatter", "nuke.createNode(\'EdgeScatter\')")
+n.addCommand("Gradient Blur", "nuke.createNode(\"GradientBlur.nk\")")
+n.addCommand("Weighted Blur", "nuke.createNode(\"WeightedBlur.nk\")")
 
 n = m.addMenu("&Time")
 n.addCommand( 'Hold at Current Frame', 'nuke.createNode("FrameHold")["first_frame"].setValue( nuke.frame() )', 'alt+h', icon="FrameHold.png")
 
-n = m.addMenu("&Color")
-n.addCommand("Lin2Log Wrapper", "nuke.createNode(\"LinLogWrapper.nk\")")
-
-n = m.addMenu("&Filter")
-n.addCommand("Gradient Blur", "nuke.createNode(\"GradientBlur.nk\")")
-n.addCommand("Weighted Blur", "nuke.createNode(\"WeightedBlur.nk\")")
-n.addCommand("Edge Scatter", "nuke.createNode(\'EdgeScatter\')")
-n.addCommand("Arri Alexa Grain", "nuke.createNode(\'ScannedGrainIH\')")
-
 n=m.addMenu("&Utility")
-n.addCommand('Named Auto Backdrop', lambda: autoBackdrop.autoBackdrop(), 'alt+b')
 n.addCommand('Backdrop Color OCD', 'backdropColorOCD()')
+n.addCommand("Build CC Nodes", "build_cc_nodes()", "alt+c")
+n.addCommand("DAG/scale Nodes up", "scaleDagNodes.scaleNodes(1.1)","alt+=")
+n.addCommand("DAG/scale Nodes down", "scaleDagNodes.scaleNodes(.9)","alt+-")
+n.addCommand("Link Roto to Tracker","trackLinker.linkTrackToRoto()","alt+o")
 n.addCommand("Make Write Directory", "make_dir_path()", "ctrl+F8")
+n.addCommand('Named Auto Backdrop', lambda: autoBackdrop.autoBackdrop(), 'alt+b')
+n.addCommand("Open Count Sheet", "open_count_sheet()")
+n.addCommand("OpticalFlare Auto Multitrack", "vclf_multi_autotrack()")
+n.addCommand("Precomp Write", "precomp_write()", "alt+p")
+n.addCommand("Quick Label", "quickLabel()", "ctrl+alt+N")
+n.addCommand("Roto/How is your paint going?","makeSad()")
+n.addCommand("Roto/RotoShapes To Trackers", "RotoShapes_to_trackers.RotoShape_to_Trackers()")
+n.addCommand("Roto/Set name to roto paint stroke range","setNameToStrokesRange()")
+n.addCommand("Set Enabled Range from Name","setEnabledByName()")
 n.addCommand("Slate/Slate and Burn-In", "nuke.createNode(\'ML_SlateBurnIn\')")
 n.addCommand("Slate/Slate Only", "nuke.createNode(\'ML_SlateOnly\')")
 n.addCommand("Viewer Input", "nuke.createNode(\'VIEWER_INPUT')")
-n.addCommand("Roto/Set name to roto paint stroke range","setNameToStrokesRange()")
-n.addCommand("Set Enabled Range from Name","setEnabledByName()")
-n.addCommand("Roto/How is your paint going?","makeSad()")
-n.addCommand("Link Roto to Tracker","trackLinker.linkTrackToRoto()","alt+o")
-n.addCommand("DAG/scale Nodes up", "scaleDagNodes.scaleNodes(1.1)","alt+=")
-n.addCommand("DAG/scale Nodes down", "scaleDagNodes.scaleNodes(.9)","alt+-")
-n.addCommand("Roto/RotoShapes To Trackers", "RotoShapes_to_trackers.RotoShape_to_Trackers()")
-n.addCommand("Quick Label", "quickLabel()", "ctrl+alt+N")
-n.addCommand("OpticalFlare Auto Multitrack", "vclf_multi_autotrack()")
-n.addCommand("Build CC Nodes", "build_cc_nodes()", "alt+c")
-n.addCommand("Open Count Sheet", "open_count_sheet()")
-n.addCommand("Precomp Write", "precomp_write()", "alt+p")
 
 # Swap out the default version up functionality with a method that makes the directory
 # stored in the Write node.
