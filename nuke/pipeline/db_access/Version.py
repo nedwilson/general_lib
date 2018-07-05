24b9ce4e1770178475115e1999447ae8f25a0a93
Ned Wilson <nedwilson@gmail.com>
2018-07-05 12:52:49 -0700

    Add g_delivered, g_playlists, and g_client_code fields to version object. This allows the pipeline to determine if the current version has already been delivered to the client, and if so, require the user to upversion.

--- nuke/pipeline/db_access/Version.py
+++ nuke/pipeline/db_access/Version.py
@@ -34,8 +34,17 @@ class Version():
         self.g_artist = m_artist
         self.g_task = m_task
         self.g_status = 'rev'
+        self.g_delivered = False
+        self.g_client_code = None
+        self.g_playlists = None
     def set_status(self, m_status):
         self.g_status = m_status
+    def set_delivered(self, m_delivered):
+        self.g_delivered = m_delivered
+    def set_client_code(self, m_client_code):
+        self.g_client_code = m_client_code
+    def set_playlists(self, m_playlists):
+        self.g_playlists = m_playlists
     def __str__(self):
         ret_str = """class Version():
     Version Name: {g_version_code} 
@@ -50,6 +59,9 @@ class Version():
     Artist: {g_artist}
     Task: {g_task}
     Status: {g_status}
+    Delivered: {g_delivered}
+    Client Version Name: {g_client_code}
+    Playlist(s): {g_playlists}
 """.format(g_version_code = self.g_version_code, 
            g_dbid = self.g_dbid,
            g_description = self.g_description,
@@ -61,6 +73,9 @@ class Version():
            g_shot = self.g_shot.g_shot_code,
            g_artist = self.g_artist.g_full_name,
            g_task = self.g_task.g_task_name,
-           g_status = self.g_status)
+           g_status = self.g_status,
+           g_delivered = self.g_delivered,
+           g_client_code = self.g_client_code,
+           g_playlists = ', '.join([pl.g_playlist_name for pl in self.g_playlists]))
         return ret_str
 
