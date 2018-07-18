fd278e5f79f175882d363e44ff84f01aab9b0db2
Ned Wilson <ned@qppe.tech>
2018-07-18 11:19:40 -0700

    Default CC file, with default values for slope, offset, power, and saturation.

---
+++ nuke/pipeline/default.cc
@@ -0,0 +1,11 @@
+<?xml version="1.0" ?>
+<ColorCorrection id="default">
+  <SOPNode>
+    <Slope>1.0 1.0 1.0</Slope>
+    <Offset>0.0 0.0 0.0</Offset>
+    <Power>1.0 1.0 1.0</Power>
+  </SOPNode>
+  <SatNode>
+    <Saturation>1.0</Saturation>
+  </SatNode>
+</ColorCorrection>
