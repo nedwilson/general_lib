b47263404f4c34fe006408479f99ef3ce5d99ead
Ned Wilson <ned@qppe.tech>
2018-07-26 13:19:22 -0700

    Added g_pipeline_step_id instance variable and a setter method to set it.

--- nuke/pipeline/db_access/Task.py
+++ nuke/pipeline/db_access/Task.py
@@ -16,6 +16,7 @@ class Task():
         self.g_status = m_status
         self.g_shot = m_shot
         self.g_dbid = m_dbid
+        self.g_pipeline_step_id = -1
 
     def __str__(self):
         ret_str = """class Task():
@@ -24,10 +25,15 @@ class Task():
     Status: {g_status}
     Shot: {g_shot}
     DBID: {g_dbid}
+    Pipeline Step ID: {g_pipeline_step_id}
 """.format(g_task_name = self.g_task_name,
            g_artist = self.g_artist.g_full_name,
            g_status = self.g_status,
            g_shot = self.g_shot.g_shot_code,
-           g_dbid = self.g_dbid)
+           g_dbid = self.g_dbid,
+           g_pipeline_step_id = self.g_pipeline_step_id)
         return ret_str
+        
+    def set_pipeline_step_id(self, m_pipeline_step_id):
+        self.g_pipeline_step_id = m_pipeline_step_id
 
