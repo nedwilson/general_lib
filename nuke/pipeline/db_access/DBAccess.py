b1e9b1b7600d9851473249ee26e9e74ea61a89a9
Ned Wilson <ned@glenlivet.qppe.tech>
2018-10-26 17:37:07 -0700

    Local copy preferred

--- nuke/pipeline/db_access/DBAccess.py
+++ nuke/pipeline/db_access/DBAccess.py
@@ -14,6 +14,10 @@ class DBAccess(object):
         """Returns a shot object from the database"""
 
     @abc.abstractmethod
+    def fetch_shot_from_id(self, m_shot_id):
+        """Returns a shot object from the database using the database ID as a search parameter"""
+
+    @abc.abstractmethod
     def fetch_plate(self, m_plate_name, m_shot_obj):
         """Returns a plate object from the database"""
 
@@ -22,15 +26,105 @@ class DBAccess(object):
         """Returns a sequence object from the database"""
 
     @abc.abstractmethod
+    def fetch_artist(self, m_full_name):                
+        """Returns an artist object from the database"""
+
+    @abc.abstractmethod
+    def fetch_artist_from_username(self, m_username):                
+        """Returns an artist object from the database"""
+
+    @abc.abstractmethod
+    def fetch_artist_from_id(self, m_artist_id):                
+        """Returns an artist object from the database using the database ID as a search parameter"""
+
+    @abc.abstractmethod
+    def fetch_tasks_for_shot(self, m_shot_obj):
+        """Returns an array of task objects assosciated with the shot object from the database"""
+
+    @abc.abstractmethod
+    def fetch_task_from_id(self, m_task_id, m_shot_obj):
+        """Returns a task object assosciated with the shot object from the database using the database ID as a search parameter"""
+
+    @abc.abstractmethod
+    def update_task_status(self, m_task_obj):
+        """Sets a status on an existing task."""
+
+    @abc.abstractmethod
+    def update_version_status(self, m_version_obj):
+        """Sets a status on an existing version."""
+
+    @abc.abstractmethod
+    def update_version_matte_delivered(self, m_version_obj):
+        """Sets matte delivered to true on an existing version."""
+        
+    @abc.abstractmethod
+    def update_shot_status(self, m_shot_obj):
+        """Sets a status on an existing shot."""
+            
+    @abc.abstractmethod
+    def fetch_version(self, m_version_name, m_shot_obj):
+        """Returns a version object from the database"""
+
+    @abc.abstractmethod
+    def fetch_version_from_id(self, m_version_id):
+        """Returns a version object from the database using the database ID as a search parameter"""
+
+    @abc.abstractmethod
+    def fetch_versions_with_status(self, m_status):
+        """Returns a list of versions in the database matching the status provided"""
+
+    @abc.abstractmethod
+    def fetch_versions_with_mattes(self):
+        """Returns a list of versions in the database with mattes ready to deliver"""
+
+    @abc.abstractmethod
+    def fetch_versions_for_shot(self, m_shot_obj):
+        """Returns a list of versions in the database that have been submitted for a particular shot"""
+
+    @abc.abstractmethod
+    def fetch_playlist(self, m_playlist_name):                
+        """Returns a playlist object from the database"""
+
+    @abc.abstractmethod
+    def fetch_notes_for_version(self, m_version_obj, b_populate_playlists=False):                
+        """Returns a list of Note objects linked to a particular version. By default, it will not attempt
+           to fill playlist objects with a database record for each version. However, setting b_populate_playlists 
+           to True will turn this on. Warning, it's slow."""
+
+    @abc.abstractmethod
+    def create_playlist(self, m_playlist_obj):
+        """Creates a playlist based on the object provided, and populates the object with the resulting database query
+           If the object already exists, with a g_dbid value != -1, method will perform update instead of create."""
+
+    @abc.abstractmethod
+    def delete_playlist(self, m_playlist_obj):
+        """Deletes a playlist with id matching m_playlist_obj.g_dbid"""
+        
+    @abc.abstractmethod
     def create_sequence(self, m_seq_obj):
         """Creates a sequence based on the object provided, and populates the object with the resulting database query"""
 
     @abc.abstractmethod
+    def create_task(self, m_task_obj):
+        """Creates a task based on the object provided, and populates the object with the resulting database query"""
+
+    @abc.abstractmethod
     def create_shot(self, m_shot_obj):
         """Creates a shot based on the object provided, and populates the object with the resulting database query"""
-            
+
+    @abc.abstractmethod
+    def create_version(self, m_version_obj):
+        """Creates a version based on the object provided, and populates the object with the resulting database query"""
+                
     @abc.abstractmethod
     def create_plate(self, m_plate_obj):
         """Creates a plate based on the object provided, and populates the object with the resulting database query"""
+
+    @abc.abstractmethod
+    def create_note(self, m_note_obj):
+        """Creates a note based on the object provided, and populates the object with the resulting database query"""
         
+    @abc.abstractmethod
+    def upload_thumbnail(self, m_entity_type, m_entity, m_thumb_path):
+        """Uploads a thumbnail to the database and attaches it to a specific entity"""
         
